56169abc8d63a06fcbb05a004208babcf77d79c9
luctran23 <anhvanluc1999@gmail.com>
2020-06-05 00:03:24 +0700

    create MVC

--- controller/news.controller.js
+++ controller/news.controller.js
@@ -1,4 +1,5 @@
 var db = require('../db');
+var shortid = require('shortid');
 
 module.exports.app = function(req, res){
     res.render('news/allNews', {
@@ -14,4 +15,43 @@ module.exports.search = function(req, res){
     res.render('news/allNews', {
         news: matchesNews
     })
+};
+
+module.exports.create = function(req, res) {
+    res.render('news/create');
+};
+module.exports.view = function(req, res) {
+    var id =  req.params.id;
+    var aNews = db.get('news').find({ id: id}).value(); 
+    res.render('news/view', {
+        news: aNews
+    })
+};
+module.exports.delete = function(req, res) {
+    var id =  req.params.id;
+    var aNews = db.get('news').find({ id: id}).value();
+    var remainNews = db.get('news').splice(db.get('news').indexOf(aNews), 1).write();
+    res.redirect('/news');
+};
+
+module.exports.modify = function(req, res) {
+    var id =  req.params.id;
+    var aNews = db.get('news').find({ id: id}).value();
+    res.render('news/modify', {
+        news: aNews
+    });
+};
+
+module.exports.postModify = function(req, res) {
+    var id =  req.params.id;
+    var aNews = db.get('news').find({ id: id}).value();
+    aNews.title = req.body.title;
+    aNews.description = req.body.description;
+    res.redirect('/news');
+};
+
+module.exports.postCreate = function(req, res) {
+    req.body.id = shortid.generate();
+    db.get('news').push(req.body).write();
+    res.redirect('/news');
 };
--- routes/news.route.js
+++ routes/news.route.js
@@ -9,44 +9,18 @@ var shortid = require('shortid');
 router.get('/', controller.app);
 
 router.get('/search', controller.search);
-router.get('/create', function(req, res) {
-    res.render('news/create');
-});
+router.get('/create', controller.create);
 
 
 
-router.get('/:id', function(req, res) {
-    var id =  req.params.id;
-    var aNews = db.get('news').find({ id: id}).value(); 
-    res.render('news/view', {
-        news: aNews
-    })
-});
+router.get('/:id', controller.view);
 
-router.get('/:id/delete', function(req, res) {
-    var id =  req.params.id;
-    var aNews = db.get('news').find({ id: id}).value();
-    var remainNews = db.get('news').splice(db.get('news').indexOf(aNews), 1).write();
-    res.redirect('/news');
-});
-router.get('/:id/modify', function(req, res) {
-    var id =  req.params.id;
-    var aNews = db.get('news').find({ id: id}).value();
-    res.render('news/modify', {
-        news: aNews
-    });
-});
-router.post('/:id/modify', function(req, res) {
-    var id =  req.params.id;
-    var aNews = db.get('news').find({ id: id}).value();
-    aNews.title = req.body.title;
-    aNews.description = req.body.description;
-    res.redirect('/news');
-})
-router.post('/create', function(req, res) {
-    req.body.id = shortid.generate();
-    db.get('news').push(req.body).write();
-    res.redirect('/news');
-});
+router.get('/:id/delete', controller.delete);
+
+router.get('/:id/modify', controller.modify);
+
+router.post('/:id/modify', controller.postModify);
+
+router.post('/create', controller.postCreate);
 
 module.exports = router;
